ebe3f591a9da92fa93155163ca9e7ca42a0295c2
Ossian Eriksson <ossian.eriksson@elev.ga.ntig.se>
2022-08-17 10:54:13 +0200

    Update Program.cs

--- Motorex Prototyp/Program.cs
+++ Motorex Prototyp/Program.cs
@@ -1,2 +1,50 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+
+public class Challenge
+{
+    private float points = 0;
+    private string challName = string.Empty;
+    private string challTask = string.Empty;
+
+    public Challenge (float points, string challName, string challTask)
+    {
+        this.points = points;
+        this.challName = challName;
+        this.challTask = challTask;
+    }
+}
+
+
+
+class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Welcome to Motorex Daily Challenge App\n\n");
+        Console.WriteLine("1. Receive Daily Challenge\n");
+        Console.WriteLine("2. Global Leaderboard\n");
+        Console.WriteLine("3. Review Previous Challenges\n");
+        Console.WriteLine("4. Exit App\n");
+
+        switch (Convert.ToInt32(Console.ReadLine()))
+        {
+            case 1://Daily challenge
+                break;
+
+            case 2://Global leaderboard
+                break;
+
+            case 3://review previous
+                break;
+
+            case 4://exit program
+                return;
+
+        }
+
+
+
+
+    }
+}
