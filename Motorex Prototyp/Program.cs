9fd18d85738b7e1cc7c05e924a0c62da6b1d69d8
Ossian Eriksson <ossian.eriksson@elev.ga.ntig.se>
2022-08-17 12:19:37 +0200

    La till något vet inte

--- Motorex Prototyp/Program.cs
+++ Motorex Prototyp/Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-
+using System.Collections.Generic;
 
 public class Challenge
 {
@@ -15,30 +15,48 @@ public class Challenge
     }
 }
 
-
-
-class Program
+public class User
 {
-    public static void Main()
+    private string Name = string.Empty; 
+    private int UserId = 0;//add friends through this 
+    private float points;
+    
+}
+
+
+
+class Program 
+{
+    public static void Main(string v)
     {
+       string fullPath = Path.GetFullPath(v);
+        IDictionary<string, string> logInPairs = new Dictionary<string, string>();
+        StreamWriter streamWriter = new StreamWriter(fullPath);
+        
+
         Console.WriteLine("Welcome to Motorex Daily Challenge App\n\n");
         Console.WriteLine("1. Receive Daily Challenge\n");
-        Console.WriteLine("2. Global Leaderboard\n");
+        Console.WriteLine("2. Leaderboards\n");
         Console.WriteLine("3. Review Previous Challenges\n");
         Console.WriteLine("4. Exit App\n");
 
         switch (Convert.ToInt32(Console.ReadLine()))
         {
             case 1://Daily challenge
+                Console.WriteLine();
                 break;
 
-            case 2://Global leaderboard
+            case 2://leaderboards friends and global add friends 
+                Console.WriteLine();
                 break;
 
             case 3://review previous
+                Console.WriteLine();
                 break;
 
             case 4://exit program
+                Console.WriteLine();
+                Console.ReadKey();
                 return;
 
         }
