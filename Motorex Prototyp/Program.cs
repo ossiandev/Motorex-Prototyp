2af0d52bbe4ba927d367fdb935b0533ddb91fdb2
Ossian Eriksson <ossian.eriksson@elev.ga.ntig.se>
2022-08-17 13:16:04 +0200

    Update Program.cs

--- Motorex Prototyp/Program.cs
+++ Motorex Prototyp/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 
+//setup global
 public class Challenge
 {
     private float points = 0;
@@ -19,7 +20,7 @@ public class User
 {
     private string Name = string.Empty; 
     private int UserId = 0;//add friends through this 
-    private float points;
+    private float points = 0;
     
 }
 
@@ -29,20 +30,22 @@ class Program
 {
     public static void Main()
     {
-        string fullPath = Path.GetFullPath("savedNames.txt");
-        IDictionary<string, string> logInPairs = new Dictionary<string, string>();
-        StreamWriter streamWriter = new StreamWriter(fullPath);
-        
+        //setup local
+        string fullPath = Path.GetFullPath("savedNames.txt"); // filepath for savedNames.txt
+        IDictionary<string, string> logInPairs = new Dictionary<string, string>(); // hashmap used for logins
+        StreamWriter streamWriter = new StreamWriter(fullPath); //used for saving and loading usernames, challenges etc.
+        streamWriter.WriteLine("hello!");
         List<Challenge> tasks = new List<Challenge> ();
         tasks.Add(new Challenge(20, "Walk A Bunch!", "Walk 10,000 steps!"));
+        List<User> users = new List<User> ();
         
-
+        //start
         Console.WriteLine("Welcome to Motorex Daily Challenge App\n\n");
         Console.WriteLine("1. Receive Daily Challenge\n");
         Console.WriteLine("2. Leaderboards\n");
         Console.WriteLine("3. Review Previous Challenges\n");
         Console.WriteLine("4. Exit App\n");
-
+        start:
         switch (Convert.ToInt32(Console.ReadLine()))
         {
             case 1://Daily challenge
@@ -51,7 +54,34 @@ class Program
                 break;
 
             case 2://leaderboards friends and global add friends 
-                Console.WriteLine();
+                Console.WriteLine("Leaderboards \n\n 1. Friend Leaderboard \n\n 2. Global Leaderboard");
+                switch (Convert.ToInt32(Console.ReadLine()))
+                {
+                    case 1:
+                        Console.WriteLine("Friend Leaderboard");
+                        int counter = 0;
+                        foreach (Object user in users )
+                        {
+                            Console.WriteLine(users);
+                            counter++;
+
+                        }
+                        Console.WriteLine("");
+                        Console.WriteLine("");
+                        Console.WriteLine("");
+
+                        if(Console.ReadLine() == "3.")
+                        {
+                            break;
+                        }
+                        break;
+                    case 2:
+                        Console.WriteLine("");
+                        Console.WriteLine("");
+                        Console.WriteLine("");
+                        Console.WriteLine("");
+                        break;
+                }
                 break;
 
             case 3://review previous
@@ -62,6 +92,8 @@ class Program
                 Console.WriteLine();
                 Console.ReadKey();
                 return;
+            default:
+                goto start;
 
         }
 
